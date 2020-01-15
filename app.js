e1b9a7f5678f97b1c752991e5911406ff3bf4eca
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2020-01-15 23:30:07 +0530

    Use let keyword

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const music = document.querySelectorAll(".music");
 const pads = document.querySelectorAll(".pads div");
 const visual = document.querySelector(".motion");
-console.log(music)
 
-    for(var i = 0; i < pads.length; i++){
-        console.log(pads[i]);
-        pads[i].addEventListener("click", function(){
+
+    for(let i = 0; i < pads.length; i++){
+        console.log(music[i]);
+        pads[i].addEventListener("click", function() {
             music[i].currentTime = 0;
             music[i].play();
         });
